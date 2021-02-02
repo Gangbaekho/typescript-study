5b069f510eb99b4839ce418549118fe78d8ad817
kimjinsoo <roorooro9933@gmail.com>
2021-02-02 18:01:11 +0900

    understanding tuple type

--- app.ts
+++ app.ts
@@ -1,19 +1,15 @@
-const person = {
+// Tuple은 length, type이 정해져있는
+// 특수한 Array라고 생각하면 됨
+// 아래의 role 같은 경우는 반드시 저 포맷으로
+// 이루어져야 한다는 것임.
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "jinsoo",
   age: 29,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-// 재밋는건 hobby가 hobbies의 item이니고
-// hobbies가 string[] 이기 떄문에
-// hobby가 string이라고 typescript는 알 수 있다.
-// 그렇기 때문에 string method가 아닌 것은 error를 낸다는 것이지.
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase);
-  // map은 array에서 지원하니까 hobby는 안된다.
-  // 밑에꺼 error를 띄운다는 거임.
-  // console.log(hobby.map((item)=>item+1));
-}
