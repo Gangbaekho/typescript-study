a7dfc7d0bff57e1fc1d9dcbac86e22ff64b79b55
kimjinsoo <roorooro9933@gmail.com>
2021-02-02 18:04:47 +0900

    understanding enum in typescript

--- app.ts
+++ app.ts
@@ -2,14 +2,32 @@
 // 특수한 Array라고 생각하면 됨
 // 아래의 role 같은 경우는 반드시 저 포맷으로
 // 이루어져야 한다는 것임.
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "jinsoo",
+//   age: 29,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "jinsoo",
   age: 29,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  // role의 string 값에 따라 뭔가 분기를 칠건데
+  // 이건 상당히 위험하다는 거지.
+  // Java 같은 경우 이럴 때, ENUM을 이용하는데
+  // Typescript는 ENUM을 추가했다는거다.
+  // role: "READ ONLY USER",
+  role: Role.READ_ONLY,
 };
