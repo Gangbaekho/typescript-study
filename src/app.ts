d0e0cf0bf9e17bc644262f9e1381fc3765ff5675
kimjinsoo <roorooro9933@gmail.com>
2021-02-08 10:23:03 +0900

    understanding getter and setter

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ class Department {
   // private name: string;
   // private employees: string[] = [];
 
-  constructor(private readonly id: string, private name: string, protected employees: string[]) {
+  constructor(
+    private readonly id: string,
+    private name: string,
+    protected employees: string[]
+  ) {
     // this.id = id;
     // this.name = name;
     // this.employees = employees;
@@ -14,39 +18,75 @@ class Department {
   }
 
   addEmployee(employee: string) {
-    this.employees.push(employee)
+    this.employees.push(employee);
   }
 
   printEmplyeeInformation() {
-    console.log(this.employees.length)
-    console.log(this.employees)
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 class ITDepartment extends Department {
-
   private admins: string[];
 
   constructor(id: string, name: string, employees: string[], admins: string[]) {
-    super(id, name, employees)
+    super(id, name, employees);
     this.admins = admins;
   }
 
   showAdmins() {
-    console.log(this.admins)
+    console.log(this.admins);
   }
 
   addEmployee(employee: string) {
-    console.log(employee)
-    this.employees.push(employee)
+    console.log(employee);
+    this.employees.push(employee);
   }
-
 }
 
-const test = new ITDepartment('t1', 'samsung', [], []);
-test.addEmployee('jinsoo')
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private reports: string[];
 
+  constructor(
+    id: string,
+    name: string,
+    employees: string[],
+    reports: string[]
+  ) {
+    super(id, name, employees);
+    this.lastReport = reports[0];
+    this.reports = reports;
+  }
 
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
 
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
 
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value");
+    }
+    this.addReport(value);
+  }
+}
 
+const test = new AccountingDepartment(
+  "a1",
+  "accountDepartment",
+  [],
+  ["recentReport"]
+);
+
+console.log(test.mostRecentReport);
+test.mostRecentReport = "test";
+console.log(test.mostRecentReport);
