b010c1910304fc36b281d19abe3a6eb6ce827188
kimjinsoo <roorooro9933@gmail.com>
2021-02-10 16:32:49 +0900

    understanding type casting when use document apu

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,4 @@
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.runningSpeed;
-  }
-  console.log(`Moving with speed : ${speed}`);
-}
-
-moveAnimal({ type: "bird", flyingSpeed: 10 });
+const userInputElement = <HTMLInputElement>(
+  document.getElementById("user-input")!
+);
+userInputElement.value = "Hi there";
