66ba5c727381673a62b4aa009497b10a99ae73a4
kimjinsoo <roorooro9933@gmail.com>
2021-02-10 15:40:51 +0900

    playing with interface

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,8 @@
-// interface Person {
-//   name: string;
-//   age: number;
+interface Named {
+  readonly name: string;
+}
 
-//   greet(phrase: string): void;
-// }
-
-// let user1: Person;
-
-// user1 = {
-//   name: "jinsoo",
-//   age: 12,
-//   //   greet(phrase: string) {
-//   //     console.log(`${phrase} ${this.name}`);
-//   //   },
-//   greet: (phrase: string) => {
-//     console.log(`${phrase} hello`);
-//   },
-// };
-
-// user1.greet("my name is");
-
-interface Greetable {
-  name: string;
-
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
