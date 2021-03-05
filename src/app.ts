b9b29d121b3d8ba371bd0e87b982f90ed694cfde
kimjinsoo <roorooro9933@gmail.com>
2021-03-05 11:42:24 +0900

    understanding generic constraint

--- src/app.ts
+++ src/app.ts
@@ -20,9 +20,9 @@ const namesOne: Array<string> = [];
 //     console.log(error);
 //   });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: "max" }, { age: 30 });
+const mergedObj = merge({ name: "max" }, { age: 10 });
 console.log(mergedObj.name);
