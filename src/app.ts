fa1ae517f2b136e4f86d956138cc90f310c55ccf
kimjinsoo <roorooro9933@gmail.com>
2021-03-06 11:49:44 +0900

    accessor decorator practice

--- src/app.ts
+++ src/app.ts
@@ -19,13 +19,49 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger("Hello this is test log message!")
-@WithTemplate("<h1>My Person Object</h1>", "app")
-class DummyPerson {
-  name = "Max";
+// @WithTemplate("<h1>My Person Object</h1>", "app")
+// class DummyPerson {
+//   name = "Max";
 
-  constructor() {
-    console.log("Creating person object");
-  }
+//   constructor() {
+//     console.log("Creating person object");
+//   }
+// }
+
+// const pers = new DummyPerson();
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
 }
 
-const pers = new DummyPerson();
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid prive - should be positive number");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
