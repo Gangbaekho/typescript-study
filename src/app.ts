d1e327594c05165acf35aae8efc6183d7903e3d3
kimjinsoo <roorooro9933@gmail.com>
2021-03-06 11:31:21 +0900

    adding WithTemplate decorator

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,21 @@ function Logger(logString: string) {
   };
 }
 
-@Logger("Hello this is test log message!")
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    console.log("What is the p?");
+    console.log(p);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Hello this is test log message!")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class DummyPerson {
   name = "Max";
 
